194fb3cab894e2562ab7c581b4617cfa2b1800a2
imungikar <imungikar@gmail.com>
2021-12-02 15:22:08 -0500

    commit

---
+++ USACO_Bronze_Problems/CowGymnastics.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <set>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    freopen("gymnastics.in", "r", stdin);
+	freopen("gymnastics.out", "w", stdout);
+    int arr[15][25]; //first index row, second index position
+    int N, K;
+    cin >> K >> N;
+    for(int i=0;i<K;i++){
+        for(int j=0;j<N;j++){
+            int temp;
+            cin >> temp;
+            temp--;
+            arr[i][temp]=j;//the number is just a name, use it as an indice to find the rank[5,4,0,1,2,3] becomes [2,3,5,4,1,0], 5th(technically 6th) is rank 0, etc
+        }
+    }
+    int answer = 0;
+    for(int i=0;i<N;i++){
+        //i is the first cow, j is the second cow
+        for(int j=i+1;j<N;j++){
+            bool first = false;
+            if(arr[0][j] > arr[0][i]){
+                first = true;
+            }
+            bool consistent = true;
+            for(int k=1;k<K;k++){
+                bool temp = false;
+                if(arr[k][j] > arr[k][i]){
+                    temp = true;
+                } if(temp != first){
+                    consistent = false;
+                    break;
+                }
+            }
+        }
+        if(consistent = true){
+            answer++;
+        }
+    }
+    cout << answer;
+}
