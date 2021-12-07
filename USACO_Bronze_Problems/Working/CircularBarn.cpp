5515031bbb6e5d2baa355a8dc91a6ebf48934a74
imungikar <imungikar@gmail.com>
2021-12-06 22:06:02 -0500

    commit

--- USACO_Bronze_Problems/Working/CircularBarn.cpp
+++ USACO_Bronze_Problems/Working/CircularBarn.cpp
@@ -2,22 +2,26 @@
 #include <cstdio>
 
 using namespace std;
+//problem solving strat --> print out everything in a loop to see if it's a logic issue
+
+
 
 int main(){
     freopen("cbarn.in", "r", stdin);
     freopen("cbarn.out", "w", stdout);
     int N;
     cin >> N;
-    int barn[N+1];
-    for(int i=1;i<N+1;i++){
+    int barn[N];
+    int ans = 0;
+    int sum = 0;
+    for(int i=0;i<N;i++){
         cin >> barn[i];
-        int sum = 0;
-        int ans = 0;
-        for(int j=1;j<N+1;j++){
+        for(int j=0;j<N;j++){
             sum += (abs(j-i))*(barn[j]);
-            ans = max(ans,sum);
+            int ans = min(ans,sum);
+            cout << ans << "\n";
         }
-        cout << ans;
+    
     }
 
 }
