1bd69c9ea12e10e009e4b64275b69931dbc5544a
imungikar <imungikar@gmail.com>
2021-12-14 23:29:18 -0500

    commit

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 - Circular barn should be right but has some weird memory leak I don't totally understand
  - Life guards tomorrow
  - Confused with whydidthecowcrosstheroad2
+ - I'm pretty sure sleepy herding is right, but there's gotta be some sort of massive syntax error
  
---
+++ USACO_Bronze_Problems/Working/SleepyCowHerding.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    vector<int> bem[2];
+    for(int i=0;i<3;i++){
+        cin >> bem[i];
+    }
+    sort(bem.begin(),bem.end());
+    int minvalue = 0;
+    if(bem[2]==bem[0]+2){//consecutive case
+        minvalue = 0;
+    } else if(bem[2]=bem[1]+2 || bem[1]=bem[0]+2){
+        minvalue = 1;
+    }else{
+        minvalue = 2;
+    }
+    int cbdist = bem[2]-bem[1];
+    int badist = bem[1]-bem[0];
+    int maxvalue = 0;
+    maxvalue = max(cbdist,badist);
+    cout << minvalue << "\n";
+    cout << maxvalue;
+
+    
+
+}
