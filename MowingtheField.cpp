881368aaa42c21248d9ef4d1c1f0dd133c991955
imungikar <imungikar@gmail.com>
2022-01-03 19:04:55 -0500

    finished field

--- MowingtheField.cpp
+++ MowingtheField.cpp
@@ -3,28 +3,56 @@
 using namespace std;
 
 int main(){
+    freopen("mowing.in", "r", stdin);
+	freopen("mowing.out", "w", stdout);
     int N;
     cin >> N;
-    int grid[21][21];
+    int grid[2001][2001];
     string inputslength[100];
     int inputsdir[100];
     for(int i=0;i<N;i++){
         cin >> inputslength[i] >> inputsdir[i];
     }
+    int lastTime[2001][2001];
+    for(int i = 0; i < 2001; i++) {
+			for(int j = 0; j < 2001; j++) {
+				lastTime[i][j] = -1;
+			}
+		}
     int lastx = 11;
     int lasty = 11;
     int time = 0;
+    int answer = 1001;
     for(int i=0;i<N;i++){
+        int k = inputsdir[i];
         if(inputslength[i]="N"){
-            lasty = lasty + inputsdir[i];
+            for(int j=0;j<k;j++){
+                lasty++;
+                time++;
+            }
         }if(inputslength[i]="E"){
-            lastx = lastx + inputsdir[i];
+            for(int j=0;j<k;j++){
+                lastx++;
+                time++;
+            }
         }if(inputslength[i]="S"){
-            lasty = lasty - inputsdir[i];
+            for(int j=0;j<k;j++){
+                lasty--;
+                time++;
+            }
         }if(inputslength[i]="W"){
-            lastx = lastx - inputsdir[i];
+            for(int j=0;j<k;j++){
+                lastx--;
+                time++;
+            }
         }
-        time += inputsdir[i];
-        if(lastx)
+        lastTime[lastx][lasty]=time;
+        if(lastTime[lastx][lasty]>=0){
+            answer = min(answer,time-lastTime[lastx][lasty]);
+        }
     }
+    if(answer==1001){
+        answer = 1;
+    }
+    cout << answer;
 }
